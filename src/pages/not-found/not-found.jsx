58e314e70f5ce31903ce121b188f62947651afc6
Claire Prin-Guénon <pringuenon.claire@gmail.com>
2025-06-23 16:14:02 +0200

    responsive page NotFound

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="Notfound">
       <p className="Notfound__404">404</p>
-      <p className="Notfound__text">Oups! La page que vous demandez n'existe pas.</p>
+      <p className="Notfound__text">Oups! La page que <span className="mobile-break"></span> vous demandez n'existe pas.</p>
       <Link to="/" className="Notfound__LinkToHome">Retourner sur la page d’accueil</Link>
     </div>
   );
